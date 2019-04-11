b3deede6b3fd081586cf95f133148458c517c880
Artem <superspy5553@mail.ru>
2019-04-11 09:26:14 +0300

    Четвертый коммит

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-'use strict';
-let question = confirm('Hello?');
+let question = prompt('Какой проект отобразить?', 'галлерея форма');
 
-console.log(question);
+
 let yes = document.querySelector('.yes');
-if (question) {
+if (question === 'галлерея') {
     yes.innerHTML = 'yes';
-} else if (!question) {
+    var forma = document.querySelector('.forma');
+    forma.classList.add('hidden');
+} else if (question === 'форма') {
     yes.innerHTML = 'not';
+    var gallery = document.querySelector('.gallery');
+    gallery.classList.add('hidden');
 }
+
 var srcPictures = [
     '3.png',
     '567.png',
@@ -23,3 +27,61 @@ picture.addEventListener('click', function() {
 for (var i = 0; i < pictures.length; i++) {
     enterImg(pictures[i], srcPictures[i]);
 }
+
+
+
+
+// Список всех сообщений
+var listMessages = document.querySelector('.listMessages');
+
+// Форма и поля текстов для ввода(названия и числа)
+var form = document.querySelector('.form');
+var input1 = form.querySelector('.message-input1');
+var input2 = form.querySelector('.message-input2');
+
+// Целое сообщение в темплате в документ-фрагменте
+var templateMessage = document.querySelector('#template').content;
+var newTemplateMessage = templateMessage.querySelector('.message');
+
+// Кнопка отправки сообщения
+var messageButton = form.querySelector('.message-button');
+
+// Кнопка удаления
+var removeMessage = function(button, element) {
+    button.addEventListener('click', function() {
+    element.remove();
+    });
+};
+
+var messages = listMessages.children;
+
+var emptyMessage = listMessages.querySelector('.emptymessage');
+
+console.log(messages.length);
+
+
+// Обработчик на отправку формы
+form.addEventListener('submit', function(evt){
+
+    evt.preventDefault();
+    var newMessage = newTemplateMessage.cloneNode(true);
+    var textNewMessage = newMessage.querySelector('.message-text');
+    textNewMessage.textContent = input1.value;
+    var numberNewMessage = newMessage.querySelector('.message-number');
+    numberNewMessage.textContent = i - 2;
+    i++;
+    var amountNewMessage = newMessage.querySelector('.message-amount');
+    amountNewMessage.textContent = input2.value;
+    var deleteButton = newMessage.querySelector('.button-remove');
+    removeMessage(deleteButton, newMessage);
+    if (textNewMessage.textContent >= 1 || textNewMessage.textContent < 1) {
+        textNewMessage.textContent = 'Ошибка!(введено число)';
+    }
+    if (Number(amountNewMessage.textContent) < 0 ) {
+        amountNewMessage.textContent = 'Ошибка!(отр. ч.)';
+        amountNewMessage.classList.add('littleclass');
+    }
+    listMessages.appendChild(newMessage);
+    input2.value = '';
+    input1.value = '';
+});
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+let question = prompt('Hello?');
